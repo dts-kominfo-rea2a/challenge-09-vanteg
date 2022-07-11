8b3c880a91c6fbd8b88c7cf2174ec81f3fa263ea
Irvan Wijaya <irvanwijaya72@gmail.com>
2022-07-11 21:20:30 +0700

    memperbaiki contact js fragment

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,26 +4,24 @@ import React from "react";
 import "../App.css";
 
 const Contact = (props) => {
-  return (
-    <>
-      {props.data.map((value) => {
-        return (
-          <>
-            <div className="felx">
-              <div>
-                <img src={value.photo} className="gambar"></img>
-              </div>
-              <div>
-                <h3>{value.name}</h3>
-                <p>{value.phone}</p>
-                <p>{value.email}</p>
-              </div>
+  {
+    props.data.map((value) => {
+      return (
+        <>
+          <div className="felx">
+            <div>
+              <img src={value.photo} className="gambar"></img>
             </div>
-          </>
-        );
-      })}
-    </>
-  );
+            <div>
+              <h3>{value.name}</h3>
+              <p>{value.phone}</p>
+              <p>{value.email}</p>
+            </div>
+          </div>
+        </>
+      );
+    });
+  }
 };
 
 export default Contact;
