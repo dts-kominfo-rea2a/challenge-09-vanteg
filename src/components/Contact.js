34cc37c2d956cd458c5f6441815945925c207d3a
Irvan Wijaya <irvanwijaya72@gmail.com>
2022-07-11 21:29:30 +0700

    memperbaiki contact js MAPclea

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,24 +4,26 @@ import React from "react";
 import "../App.css";
 
 const Contact = (props) => {
-  {
-    props.data?.map((value) => {
-      return (
-        <>
-          <div className="felx">
-            <div>
-              <img src={value.photo} className="gambar"></img>
+  return (
+    <div>
+      {props.data.map((value) => {
+        return (
+          <>
+            <div className="felx">
+              <div>
+                <img src={value.photo} className="gambar"></img>
+              </div>
+              <div>
+                <h3>{value.name}</h3>
+                <p>{value.phone}</p>
+                <p>{value.email}</p>
+              </div>
             </div>
-            <div>
-              <h3>{value.name}</h3>
-              <p>{value.phone}</p>
-              <p>{value.email}</p>
-            </div>
-          </div>
-        </>
-      );
-    });
-  }
+          </>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Contact;
