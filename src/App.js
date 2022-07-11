3ecb8531007d75c55746d1ee025fb7d55307bca2
Irvan Wijaya <irvanwijaya72@gmail.com>
2022-07-11 21:16:24 +0700

    membuat sebuah react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import './App.css';
+import "./App.css";
+import React from "react";
 
 // Uncomment untuk memuat daftar kontak
 // import contacts from './data/contacts.json';
+import contacts from "./data/contacts.json";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Header />
+      <Contact data={contacts} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,29 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import "../App.css";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = (props) => {
+  return (
+    <>
+      {props.data.map((value) => {
+        return (
+          <>
+            <div className="felx">
+              <div>
+                <img src={value.photo} className="gambar"></img>
+              </div>
+              <div>
+                <h3>{value.name}</h3>
+                <p>{value.phone}</p>
+                <p>{value.email}</p>
+              </div>
+            </div>
+          </>
+        );
+      })}
+    </>
+  );
+};
 
 export default Contact;
